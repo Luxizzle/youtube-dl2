78f0d977f01273a9cd7d7961646c84bb58e5d9ab
Jelmer <luxizzle@gmail.com>
2018-03-13 07:02:54 +0100

    Better getInfo testing

--- test/get-info.test.js
+++ test/get-info.test.js
@@ -2,40 +2,58 @@ const test = require('ava')
 
 const getInfo = require('../src/get-info')
 
-test('single video', async t => {
-  const info = await getInfo('https://www.youtube.com/watch?v=RB4nFoA63rs')
-  const keys = [
-    'title',
-    'uploader',
-    'width',
-    'extractor',
-    '_filename',
-    'duration',
-    'ext',
-    'thumbnail',
-    'thumbnails',
-    'vcodec',
-    'webpage_url',
-    'webpage_url_basename'
-  ]
-  t.log(info)
+const keys = [
+  'title',
+  'uploader',
+  'width',
+  'extractor',
+  '_filename',
+  'duration',
+  'ext',
+  'thumbnail',
+  'thumbnails',
+  'vcodec',
+  'webpage_url',
+  'webpage_url_basename'
+]
 
+function parseInfo(info) {
   const info2 = {}
 
   keys.forEach(key => {
     info2[key] = info[key]
   })
 
-  t.snapshot(info2)
+  return info2
+}
+
+test('single video', async t => {
+  let info = await getInfo('https://www.youtube.com/watch?v=RB4nFoA63rs')
+ 
+  info = parseInfo(info)
+
+  t.snapshot(info)
 })
 
 test('multiple videos', async t => {
-  t.true(Array.isArray(await getInfo([
+  let info = await getInfo([
     'https://www.youtube.com/watch?v=RB4nFoA63rs',
     'https://youtu.be/imMSZLOElBw'
-  ])))
+  ])
+
+  t.true(Array.isArray(info))
+
+  info = info.map((v) => parseInfo(v)) 
+
+  t.snapshot(info)
 })
 
 test('playlist', async t => {
-  t.true(Array.isArray(await getInfo('https://www.youtube.com/playlist?list=PL4AA2CAF1947F471A')))
+  let info = await getInfo('https://www.youtube.com/playlist?list=PL4AA2CAF1947F471A')
+
+  t.true(Array.isArray(info))
+
+  info = info.map((v) => parseInfo(v)) 
+
+  t.snapshot(info)
 })
